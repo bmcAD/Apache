5234a88a7670055e50cf5ea07abb6b7678caf92f
bmcAD <kavraham@bmc.com>
2016-02-17 16:15:28 +0200

    Update linuxOS.lua

--- modules/linuxOS.lua
+++ modules/linuxOS.lua
@@ -10,6 +10,28 @@
 local linuxOS = {}
 
 
+
+local apacheProperties = {}
+local apache_exe_path = nil
+local apache_root_directory = nil
+local serverVersion = nil
+local serverArchitecture = nil
+local serverConfigFile = nil
+local serverConfigFilePath = nil
+
+
+local fileLocation = "http://vw-tlv-ad-qa18/Apache/"
+local downloadFileDestination = ""
+local installFileDestination = ""
+
+
+local fileNameApache24_64bit_rhel7 = "rhel7-64-apache24.tar"
+local fileNameApache22_64bit_rhel7 = "rhel7-64-apache22.tar"
+local fileNameApache24_64bit_rhel6 = "rhel6-64-apache24.tar"
+local fileNameApache22_64bit_rhel6 = "rhel6-64-apache22.tar"
+local confFileNameApache22 = "bmc-aeuem-apache22.conf"
+local confFileNameApache24 = "bmc-aeuem-apache24.conf"
+
 local function execute()
 end
 
